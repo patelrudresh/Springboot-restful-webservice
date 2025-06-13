7d4bfe2345859a0ba98fb80e9a8a1f4552b5fcdc
patelrudresh <rudreshpatel728@gmail.com>
2025-06-13 20:12:09 +0530

    Dtomappet service controller class changes

--- src/main/java/com/RestApiMvc/service/impl/ServiceImpl.java
+++ src/main/java/com/RestApiMvc/service/impl/ServiceImpl.java
@@ -2,15 +2,19 @@ package com.RestApiMvc.service.impl;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.RestApiMvc.Dto.userDto;
 import com.RestApiMvc.entity.User;
+import com.RestApiMvc.mapper.userMapper;
 import com.RestApiMvc.repository.UserRepository;
 import com.RestApiMvc.service.userService;
 
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+
 
 @Service
 @AllArgsConstructor
@@ -19,36 +23,44 @@ public class ServiceImpl implements userService {
 	private UserRepository userRepository;
 
 	@Override
-	public User createUser(User user) {
-		return userRepository.save(user);
+	public userDto createUser(userDto userDto) {
+		// convert userdto into jpa entity
+		User user =userMapper.maptoUser(userDto);
+		User savedUser = userRepository.save(user);
+		// convert user jpa to userdto entity
+		userDto saveduserDto = userMapper.maptoserDto(savedUser);
+		return saveduserDto;
 	}
 
 	@Override
-	public User getUserById(Long UserId) {
+	public userDto getUserById(Long UserId) {
 
 		Optional<User> optinalUser = userRepository.findById(UserId);
-		return optinalUser.get();
+		User user= optinalUser.get();
+		return userMapper.maptoserDto(user);
 	}
 
 	@Override
-	public List<User> getAllUser() {
-		return userRepository.findAll();
+	public List<userDto> getAllUser() {
+		 List<User> user =userRepository.findAll();
+		 return user.stream().map(userMapper::maptoserDto).collect(Collectors.toList());
 
 	}
 
 	@Override
-	public User updateUser(User user) {
+	public userDto updateUser(userDto user) {
 		User existingUser = userRepository.findById(user.getId()).get();
 		existingUser.setFirstName(user.getFirstName());
 		existingUser.setLastName(user.getLastName());
 		existingUser.setEmail(user.getEmail());
 		User updatesUser = userRepository.save(existingUser);
-		return updatesUser;
+		
+		return userMapper.maptoserDto(updatesUser);
 	}
 
 	@Override
 	public void deleteUser(Long Userid) {
 		userRepository.deleteById(Userid);
-		
+
 	}
 }
